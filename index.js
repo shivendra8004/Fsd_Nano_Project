0e3413e077e89c77a626607c8b45d6b45906d24e
Shivansh_Tiwari <stshivanshtiw566@gmail.com>
2022-12-07 23:28:43 +0530

    Created server and set default values

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const http = require("http");
 const fs = require("fs");
 var requests = require("requests");
 const homeFile = fs.readFileSync("home.html", "utf-8");
+const replaceVal = (temval, orgval) => {
+    let temperature = temval.replace('{%tempval%}',orgval.main.temp);
+    temperature = temperature.replace('{%tempmin%}',orgval.main.temp_min);
+    temperature = temperature.replace('{%tempmax%}',orgval.main.temp_max);
+    temperature = temperature.replace('{%city%}',orgval.name);
+    temperature = temperature.replace('{%country%}',orgval.sys.country);
+    temperature = temperature.replace('{%tempstatus%}',orgval.weather[0].main);
+    return temperature;
+  };
